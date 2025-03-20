284515db17be1dbe385632726b1e6645c9966e1d
Chris <chris.lawes@icloud.com>
2025-03-20 11:56:27 +0000

    create new nuxt, add: default views, pc components, base settings

---
+++ nuxt.config.ts
@@ -0,0 +1,22 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    compatibilityDate: '2024-11-01',
+    devtools: {
+        enabled: false
+    },
+    ssr: false,
+    vue: {
+        compilerOptions: {
+            isCustomElement: (tag) => tag.includes('-')
+        }
+    },
+    css: ['@provetcloud/css'],
+    app: {
+        head: {
+            bodyAttrs: {
+                class: 'n-reset'
+            }
+        }
+    },
+    modules: ['@nuxt/test-utils'],
+})
